f1a59c8fece1746ace2e2b55e7e33529ab9c6a78
mercury <ho@localhost>
2026-09-08 05:58:11 -0400

    v0.0.16: matrix observatory (E2EE homeserver + sidecar), setup slots fixes, omp reports Mercury version

--- hermes/mercury_cli/__init__.py
+++ hermes/mercury_cli/__init__.py
@@ -14,8 +14,8 @@ Provides subcommands for:
 import os
 import sys
 
-__version__ = "0.0.15"
-__release_date__ = "2026.9.07"
+__version__ = "0.0.16"
+__release_date__ = "2026.9.08"
 
 
 def _ensure_utf8():
