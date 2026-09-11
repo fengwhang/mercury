6efab9d8a2e725b43c4e912467526087a4ea530c
mercury <mercury@localhost>
2026-09-11 01:23:42 -0400

    v0.0.37

--- hermes/mercury_cli/__init__.py
+++ hermes/mercury_cli/__init__.py
@@ -14,8 +14,8 @@ Provides subcommands for:
 import os
 import sys
 
-__version__ = "0.0.36"
-__release_date__ = "2026.9.11"
+__version__ = "0.0.37"
+__release_date__ = "2026.9.12"
 
 
 def _ensure_utf8():
