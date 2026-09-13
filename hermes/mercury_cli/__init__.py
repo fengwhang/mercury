4de4a050d05c3db95869171bf6fd80327480aaf2
mercury <mercury@localhost>
2026-09-13 01:23:04 -0400

    v0.0.47 version bump (omp SELF trace wave)

--- hermes/mercury_cli/__init__.py
+++ hermes/mercury_cli/__init__.py
@@ -14,8 +14,8 @@ Provides subcommands for:
 import os
 import sys
 
-__version__ = "0.0.46"
-__release_date__ = "2026.9.12"
+__version__ = "0.0.47"
+__release_date__ = "2026.9.13"
 
 
 def _ensure_utf8():
